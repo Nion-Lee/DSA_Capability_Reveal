8cc20b18b081ca983f9fee76b20a09156f77c0cc
Никола Тесла <nionismine@yahoo.com.tw>
2022-09-11 18:07:57 +0800

    加入專案檔案。

---
+++ ReplicateExecutableFilesDownloadIssue/Program.cs
@@ -0,0 +1,21 @@
+﻿using ReplicateExecutableFilesDownloadIssue;
+
+var solution = new Solution();
+var unitTests = new string[]
+{
+    "aHR0cHM6Ly93d3cuYmFzZTY0ZGVjb2RlLm9yZy8=.exe",
+    "aHR0cHM6Ly93d3cuYmFzZTY0ZGVjb2RlLm9yZy8=(1).exe",
+    "aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8=.exe",
+    "aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8=(999).exe",
+    "aHR0cHM6Ly93d3cubGlua2VkaW4uY29tL2luL25pb24tbGVlLThiMzk0YTExYi8=.exe",
+    "aHR0cHM6Ly93d3cubGlua2VkaW4uY29tL2luL25pb24tbGVlLThiMzk0YTExYi8=(12345678900).exe",
+};
+
+var encodedUrls = unitTests.Select(url =>
+{
+    solution.TrimBracketsFromBase64FileName(ref url);
+    Console.WriteLine(url);
+    return url;
+});
+
+encodedUrls.Count();
---
+++ ReplicateExecutableFilesDownloadIssue/Solution.cs
@@ -0,0 +1,37 @@
+﻿namespace ReplicateExecutableFilesDownloadIssue
+{
+    public class Solution
+    {
+        public void TrimBracketsFromBase64FileName(ref string fileName)
+        {
+            if (fileName[^5] != ')')
+                return;
+
+            int index = fileName.Length - 6;
+            while (index >= 0)
+            {
+                if (Char.IsDigit(fileName[index]))
+                {
+                    index--;
+                    continue;
+                }
+
+                if (fileName[index] == '(')
+                    break;
+            }
+
+            Span<char> chars = stackalloc char[index + 4];
+            for (int i = index - 1; i >= 0; i--)
+            {
+                chars[i] = fileName[i];
+            }
+
+            chars[index] = '.';
+            chars[index + 1] = 'e';
+            chars[index + 2] = 'x';
+            chars[index + 3] = 'e';
+
+            fileName = new string(chars);
+        }
+    }
+}
